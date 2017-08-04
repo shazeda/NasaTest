5fbad9024d49ae9af2e8ed4a974b87341b752295
Shazeda Sharmin <shazeda.sharmin1@gmail.com>
2017-08-03 21:13:39 -0400

    creating common script

---
+++ src/main/java/nasa/framework/CommonScript.java
@@ -0,0 +1,15 @@
+package nasa.framework;
+
+/**
+ * Created by shazeda on 8/3/17.
+ */
+public class CommonScript {
+
+    public final static String BASE_URL = "https://api.nasa.gov";
+    public final static String SOUND_ENDPOINT = "/planetary/sounds";
+
+    public void print(Object obj) {
+        System.out.println(obj);
+    }
+
+}
--- src/test/java/nasa/apiTest/SoundAPITest.java
+++ src/test/java/nasa/apiTest/SoundAPITest.java
@@ -3,21 +3,20 @@ package nasa.apiTest;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import nasa.framework.CommonScript;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * Created by shazeda on 7/30/17.
  */
-public class SoundAPITest {
-
-    final static String url = "https://api.nasa.gov/planetary/sounds";
+public class SoundAPITest extends CommonScript {
 
     @Test
     //Test Case ID: 1
     public void testWithFiveLimit() throws Exception {
 
-        HttpResponse<JsonNode> response = Unirest.get(url + "?q=moon&limit=5&api_key=DEMO_KEY").asJson();
+        HttpResponse<JsonNode> response = Unirest.get(BASE_URL + SOUND_ENDPOINT + "?q=moon&limit=5&api_key=DEMO_KEY").asJson();
         print(response.getStatus());
         print(response.getBody());
 
@@ -30,7 +29,7 @@ public class SoundAPITest {
     //Test Case ID: 2
     public void testWithFiftyLimit() throws Exception {
 
-        HttpResponse<JsonNode> response = Unirest.get(url + "?q=moon&limit=50&api_key=DEMO_KEY").asJson();
+        HttpResponse<JsonNode> response = Unirest.get(BASE_URL + SOUND_ENDPOINT + "?q=moon&limit=50&api_key=DEMO_KEY").asJson();
         print(response.getStatus());
         print(response.getBody());
 
@@ -43,7 +42,7 @@ public class SoundAPITest {
     //Test Case ID: 3
     public void testWithoutApiKey() throws Exception {
 
-        HttpResponse<JsonNode> response = Unirest.get(url + "?q=moon&limit=50").asJson();
+        HttpResponse<JsonNode> response = Unirest.get(BASE_URL + SOUND_ENDPOINT + "?q=moon&limit=50").asJson();
         print(response.getStatus());
         print(response.getBody());
 
@@ -57,7 +56,7 @@ public class SoundAPITest {
     //Test Case ID: 4
     public void testWithMars() throws Exception {
 
-        HttpResponse<JsonNode> response = Unirest.get(url + "?q=mars&limit=9&api_key=DEMO_KEY").asJson();
+        HttpResponse<JsonNode> response = Unirest.get(BASE_URL + SOUND_ENDPOINT + "?q=mars&limit=9&api_key=DEMO_KEY").asJson();
         print(response.getStatus());
         print(response.getBody());
 
@@ -70,7 +69,7 @@ public class SoundAPITest {
     @Test
     //Test Case ID: 5
     public void testWithoutQParameter() throws Exception {
-        HttpResponse<JsonNode> response = Unirest.get(url + "?limit=5&api_key=DEMO_KEY").asJson();
+        HttpResponse<JsonNode> response = Unirest.get(BASE_URL + SOUND_ENDPOINT + "?limit=5&api_key=DEMO_KEY").asJson();
         print(response.getStatus());
         print(response.getBody());
 
@@ -83,7 +82,7 @@ public class SoundAPITest {
     @Test
     //Test Case ID: 6
     public void testWithDefaultLimit() throws Exception {
-        HttpResponse<JsonNode> response = Unirest.get(url + "?api_key=DEMO_KEY").asJson();
+        HttpResponse<JsonNode> response = Unirest.get(BASE_URL + SOUND_ENDPOINT + "?api_key=DEMO_KEY").asJson();
         print(response.getStatus());
         print(response.getBody());
 
@@ -92,8 +91,4 @@ public class SoundAPITest {
 
     }
 
-
-    public void print(Object obj) {
-        System.out.println(obj);
-    }
 }
